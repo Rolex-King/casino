dbeeec3679fb5628b6524da92bc3c2bb5ecf1b10
Rolex-King <luisdapolixe@gmail.com>
2018-07-26 23:51:07 +0200

    Devise and new Round

---
+++ db/migrate/20180726205710_create_rounds.rb
@@ -0,0 +1,10 @@
+class CreateRounds < ActiveRecord::Migration
+  def change
+    create_table :rounds do |t|
+      t.references :user, index: true, foreign_key: true
+      t.integer :resultado
+
+      t.timestamps null: false
+    end
+  end
+end
